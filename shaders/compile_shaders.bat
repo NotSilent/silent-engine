c651c7cdfc42c4fff960aefa68a67b112f9efeb8
silent <keiya94@gmail.com>
2022-03-13 20:00:33 +0100

    Compile shaders with CMake

--- shaders/compile_shaders.bat
+++
@@ -1,3 +0,0 @@
-C:\VulkanSDK\1.2.176.1\Bin\glslc.exe C:\Users\SILENT\Desktop\silent-engine\silent-engine\shaders\unlit.vert -o unlit.vert.spv
-C:\VulkanSDK\1.2.176.1\Bin\glslc.exe C:\Users\SILENT\Desktop\silent-engine\silent-engine\shaders\unlit.frag -o unlit.frag.spv
-pause
